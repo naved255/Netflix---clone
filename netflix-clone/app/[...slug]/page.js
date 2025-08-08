affd614f1ad8db50b72cbdbc35ce123b08de5fc7
naved255 <navedahmad2314lko@gamil.com>
2025-08-08 16:48:36 +0530

    update padding

--- netflix-clone/app/[...slug]/page.js
+++ netflix-clone/app/[...slug]/page.js
@@ -12,7 +12,7 @@ const page = async ({ params })  => {
         <div className='w-full py-12 outline-1 outline-gray-400 flex justify-center items-center'>
             <Image src={`/${slug[0]}`} alt='image' width={250} height={350} />
         </div>
-        <div className='container px-4'>
+        <div className='container px-4 flex justify-center items-center py-12'>
             <p className='text-white opacity-80'>{slug[1].replaceAll('%20', ' ').replaceAll('%2C', ',')}</p>
         </div>
         <div className='w-full py-24 px-2.5 flex flex-col md:flex-row justify-center items-center gap-3'>
